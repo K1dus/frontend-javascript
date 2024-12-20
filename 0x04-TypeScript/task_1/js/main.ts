0896fcb9ba6f9c6f5829f5070321696248ff3763
K1dus <88451528+K1dus@users.noreply.github.com>
2024-12-21 00:22:17 +0300

    Adding main.ts

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,19 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Allow additional properties
+}
+
+// Example usage
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
